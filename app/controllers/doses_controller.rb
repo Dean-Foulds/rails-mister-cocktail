591553ef6641f15ff384a4d261a62332e112534a
Dean Foulds <deanfoulds@gmail.com>
2016-08-19 15:11:04 +0100

    delete method template added

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -13,7 +13,9 @@ class DosesController < ApplicationController
   end
 
   def destroy
-
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
   end
 
   private
