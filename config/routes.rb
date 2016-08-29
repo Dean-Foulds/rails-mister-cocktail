26889360b48021973e5a3a26973c7bb1edc72378
Dean Foulds <deanfoulds@gmail.com>
2016-08-29 21:12:37 +0100

    added photo option to routes and table

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:show, :index, :new, :create] do
     resources :doses, only: [:create, :destroy]
+    member do                             # member => restaurant id in URL
+      get 'photo', to: "cocktails#photo"  # RestaurantsController#chef
+    end
   end
   root "cocktails#index"
 end
